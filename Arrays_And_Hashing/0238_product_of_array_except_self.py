88aae8b31757b247376435bc74a037118e5a9ef0
lassya2507 <lassya2507@gmail.com>
2026-05-18 19:33:19 -0400

    solve: product of array except self

---
+++ Arrays_And_Hashing/0238_product_of_array_except_self.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.productExceptSelf([1, 2, 3, 4]))        # [24, 12, 8, 6]
+    print(sol.productExceptSelf([-1, 1, 0, -3, 3]))   # [0, 0, 9, 0, 0]
